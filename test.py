2228ec62a8b60b37c075524b5a6d1075a86d7352
simone tardiani <simone.tardiani@gmail.com>
2022-05-28 23:54:17 +0200

    FirstCommit

---
+++ test.py
@@ -0,0 +1,11 @@
+from ast import arg
+from http import client
+from multiprocessing import managers
+from pydoc import cli
+import socket
+import multiprocessing
+from time import sleep
+import select
+
+print("dioboia")
+sleep(100)
