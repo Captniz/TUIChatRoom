d11ad11684c29a77ebe527c38573d013d4fce187
Simone-Tardiani <simone.tardiani@gmail.com>
2022-07-20 16:31:19 +0200

    Finished the server part (Didn't test it tho)

--- test.py
+++ test.py
@@ -1,21 +1,6 @@
-import curses
-from curses import wrapper
+from ctypes import sizeof
 
+b = 1
 
-def main(stdscr):
-    stdscr.clear()
-    stdscr.refresh()
-    pad=curses.newpad(1000,200)
-    stdscr.refresh()
-
-    for i in range(2000):
-        for j in range(26):
-            char = chr(65+j)
-            pad.addstr(char)
-
-    pad.refresh(0,0,1,2,3,4)
-
-    stdscr.getch()
-    
-if __name__ == '__main__':
-    wrapper(main)     
+a = sizeof(b)
+print(f'{a}')
